8044a8f6833fa5e7ce8344fa9a378f45656fc52a
Raymundo Alvareze <raymundoar94@gmail.com>
2019-01-29 15:50:16 -0600

    se agregan dos secciones

--- Front/createandimagine/src/app/app.module.ts
+++ Front/createandimagine/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './public/index/index.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
